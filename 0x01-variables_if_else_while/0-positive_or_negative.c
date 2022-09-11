c5d3b6ac6006d41183a96577465970610c1a69ec
Justink-08 <kewelosp@gmail.com>
2022-09-11 09:47:42 -0700

    A program that assigns a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /**
- * main - print last digit in random number
+ * main - determine if number is positive or negative
  *
- * Description: print last digit in random number
+ * Description: program will assign a random number to the variable n each time it is executed.
  *
  * Return: Always 0 (Success)
  */
@@ -14,14 +14,15 @@ int main(void)
 {
 	int n;
 
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
 
-	if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last);
-	else if (n < 6 && n != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
