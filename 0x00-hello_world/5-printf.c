4aed269ade807d73ef33f04061552640cdc1e8de
Justink-08 <kewelosp@gmail.com>
2022-09-11 02:40:44 -0700

    A program that prints and print the size of data types

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - entry point
  *
- * Description - prints with proper grammar but the outcome is a piece of art
+ * Description - prints with proper grammar, but the outcome is a piece of art
  *
  * return : Always 0 (success)
  */
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
 /**
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled
- * and run on
+ * Description: prints the size of various types on the computer it is compiled and run on
  *
  * Return: Always 0 (Success)
  */
@@ -14,10 +13,8 @@ int main(void)
 	printf("size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("size of a long long int: %d by8te(s)\n", (int) sizeof(long long int));
+	printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", (int) sizeof(float));
 
 	return (0);
-
-
 }
