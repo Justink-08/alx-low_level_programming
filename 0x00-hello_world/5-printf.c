fadbef88ce90aa365e70e7e684204916c56896a8
Justink-08 <kewelosp@gmail.com>
2022-09-11 03:01:51 -0700

    A program to print and display the size of data type

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Description - prints with proper grammar, but the outcome is a piece of art
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
- * return : Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
