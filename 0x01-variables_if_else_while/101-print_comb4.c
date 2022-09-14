f0810caef34bf98385dcbcd225098b8660823d2d
Justink-08 <kewelosp@gmail.com>
2022-09-14 16:41:57 -0700

    A program to print 3 combo of numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,43 +15,36 @@
 
 	k=48;
 	e=48;
-        j=48;
+	j=48;
 
 	while (k < 58)
-
-	{
+	{	
 		e = k + 1;
 		while (e < 58)
-	       {
+		{
+			j = e + 1;
+			while (j < 58)
+			{
+				putchar(k);
+				putchar(e);
+				putchar(j);
+				if (k < 55 || e < 56 || j < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
 
-		       j = e + 1;
-		       while (j < 58)
-		     {
-			     putchar(k);
-			     putchar(e);
-			     putchar(j);
-			     if (k < 55 || e < 56 || j < 57 )
-			     {
-			     
-				     putchar(44);
-				     putchar(32);
-			     
-			     }
-			    
-			     j++;
-	
-		      } 
-		      
-		       e++;
-		
-		} 
-		
+				j++;
+			}
+
+			e++;
+		}
+
 		k++;
-	
-	
 	}
-
+	
 	putchar(10);
+
 
 	return (0);
 
