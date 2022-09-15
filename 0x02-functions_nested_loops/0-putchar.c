ac6213c75bb47e4eb379f8f0e72568de35f03eda
Justink-08 <kewelosp@gmail.com>
2022-09-15 02:42:32 -0700

    A program that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "justink.h"
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char *ke = "justink";
+
+
+
+			while (*ke)
+
+					{
+
+								_putchar(*ke);
+
+										ke++;
+
+											}
+
+				_putchar('\n');
+
+
+
+					return (0);
+
+}
