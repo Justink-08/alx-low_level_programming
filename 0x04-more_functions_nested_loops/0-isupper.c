ecd17d6025c1a94957cdb7e5f1a4168672d21c21
Justink-08 <kewelosp@gmail.com>
2022-09-15 15:32:16 -0700

    a function to return 1 after checking for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper(int c) - to check uppercase
+ *
+ * return : return (1)
+ */
+int _isupper(int c)
+{
+	int c, k;
+
+	k = 1;
+	
+	if (k > 0)
+	{
+	while (c = 'A'; c <= 'Z'; c++)
+	
+		_putchar(c);
+	}
+
+	_putchar(1);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+a function that uses _putchar
