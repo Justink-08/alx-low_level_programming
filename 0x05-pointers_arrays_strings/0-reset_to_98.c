a635b54b2cd6610b9dad1428b6663c335670d8de
Justink-08 <kewelosp@gmail.com>
2022-09-19 13:54:05 -0700

    a function that takes a pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
-
 /**
- * void reset_to_98 - afunction that takes a pointer to int
+ * reset_to_98 - afunction that takes a pointer to int
  *
  * return : (0) Always
  *
- * 
- */
-
+  */
 void reset_to_98(int *n)
 {
 	*n = 98;
