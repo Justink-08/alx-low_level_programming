0121285755f96943a4b881018b710a14af7a2183
Justink-08 <kewelosp@gmail.com>
2022-09-26 18:20:51 -0700

    a function that fills a memory byte witj constant b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset() - a function that fills a memory byte
+ * * _memset() - a function that fills a memory byte
  * @b: the value to be filled in memory
  * @n: the numberod timesto be  filled
  * @s: the memory ti fill
