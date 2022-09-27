84c63598f662f18e90f596f6b591af2030ae7f23
Justink-08 <kewelosp@gmail.com>
2022-09-26 18:07:25 -0700

    a function that copies a char to the n adddress

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset() - a function that fills a memory byte
+ * @b: the value to be filled in memory
+ * @n: the numberod timesto be  filled
+ * @s: the memory ti fill
+ *
+ * Return: s pointer.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+a funxtion that copies the char b to the memory address of s
